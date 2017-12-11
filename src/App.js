cecee101b50cd86fccb1822ea21cfb259f93158f
Shiva Venkatesh <shiva@bangthetable.com>
2017-12-11 12:27:58 +0530

    display: display the fetched text from endpoint

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
+
+import { Container, Header } from 'semantic-ui-react'
+import TextContainer from './components/text-container.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      content: ''
+    }
+    this.strip = this.strip.bind(this)
+  }
+
+  strip(html) {
+     var tmp = document.createElement("DIV");
+     tmp.innerHTML = html;
+     return tmp.textContent || tmp.innerText || "";
+  }
+
+  componentDidMount() {
+    axios.get('http://www.randomtext.me/api/')
+      .then((response) => {
+        console.log(response.data.text_out)
+        this.setState({
+          content: this.strip(response.data.text_out)
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2 className="heading">
+            Type-it.
+          </h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container text>
+          <p className="App-intro">
+            A game to measure your typing speed.
+          </p>
+          <TextContainer content={this.state.content} />
+        </Container>
       </div>
     );
   }
---
+++ src/components/text-container.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './text-container.css'
+
+const TextContainer = ({content}) => {
+  return(
+      <div className="container">
+      
+        {content}
+      </div>
+    )
+}
+
+TextContainer.defaultProps = {
+  content : "<p>Lorem ipsum euismod interdum suspendisse torquent sem arcu aenean justo, nunc fringilla odio luctus fermentum eu turpis sem hendrerit facilisis, magna curabitur posuere enim proin maecenas turpis blandit.</p> <p>Erat etiam id dolor sem mollis lobortis nisl sem scelerisque egestas, pulvinar est cursus aenean pretium varius ultricies pulvinar ac taciti ipsum eleifend tellus pretium ultricies justo dapibus quisque primis ante.</p> <p>Bibendum leo aliquam aliquet vivamus purus platea sociosqu vehicula faucibus class venenatis senectus sed nullam blandit id malesuada nunc primis sociosqu commodo orci, maecenas blandit eget rhoncus sollicitudin per habitant.</p> <p>Ultrices scelerisque curae id pretium turpis potenti porta sollicitudin netus adipiscing, aenean at sociosqu orci blandit eu ac urna augue varius venenatis eget nisl gravida quisque orci platea aliquam vivamus.</p> "
+};
+
+export default TextContainer
