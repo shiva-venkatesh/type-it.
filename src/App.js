cc4320a90f8bf3ccf9a18fcbfdd58c7516b1a2f0
Shiva Venkatesh <shiva@bangthetable.com>
2017-12-11 13:28:54 +0530

    show/hide: show hide the timer block

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import './App.css'
 
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header, Button } from 'semantic-ui-react'
 import TextContainer from './components/text-container.js'
 import InputBox from './components/input-box.js'
 
@@ -10,9 +10,13 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      content: ''
+      content: '',
+      showTimerBlock: false,
+      wordCount: 0
     }
     this.strip = this.strip.bind(this)
+    this.getWordCount = this.getWordCount.bind(this)
+    this.renderTimerBlock = this.renderTimerBlock.bind(this)
   }
 
   strip(html) {
@@ -21,17 +25,41 @@ class App extends Component {
      return tmp.textContent || tmp.innerText || "";
   }
 
+  getWordCount(content) {
+    console.log('The word count is : ' + content.split(" ").length)
+    return content.split(" ").length;
+  }
+
   componentDidMount() {
     axios.get('http://www.randomtext.me/api/')
       .then((response) => {
         console.log(response.data.text_out)
         this.setState({
-          content: this.strip(response.data.text_out)
+          content: this.strip(response.data.text_out),
+          wordCount: this.getWordCount(response.data.text_out)
         })
       })
       .catch((err) => {
         console.log(err)
       })
+  }
+
+  renderTimerBlock() {
+    if(!this.state.showTimerBlock) {
+      return false
+    }
+    else {
+      return(
+        <div className="timer-block">
+          <div className="word-count">
+            {'Word count : '  + this.state.wordCount}
+          </div>
+          <div className="typing-box">
+            <InputBox changeHandler={(e, data) => { console.log('Handled change : ' + e.key) }} />
+          </div>
+        </div>
+      )
+    }
   }
 
   render() {
@@ -47,9 +75,10 @@ class App extends Component {
             A game to measure your typing speed.
           </p>
           <TextContainer content={this.state.content} />
-          <div className="typing-box">
-            <InputBox changeHandler={(e, data) => { console.log('Handled change : ' + e.key) }} />
+          <div className={"start-button-block " + (this.state.showTimerBlock ? "hide" : "visible")}>
+            <Button primary onClick={() => { this.setState({ showTimerBlock: true }) }}>Start timer</Button>
           </div>
+          {this.renderTimerBlock()}
         </Container>
       </div>
     );
